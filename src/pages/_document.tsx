26bb3ab56dd29a9a5a5905019c13ef9a1a2c903b
Miguel Arcanjo <mig.ramos10@gmail.com>
2023-07-09 23:02:46 -0300

    cria projeto e document + font

---
+++ src/pages/_document.tsx
@@ -0,0 +1,20 @@
+import { Html, Head, Main, NextScript } from "next/document";
+
+export default function Document() {
+  return (
+    <Html lang="pt-br">
+      <Head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Poppins:wght@100;200;300;400;500;600;700;800;900&display=swap"
+          rel="stylesheet"
+        />
+        <link rel="shortcut icon" href="/favicon.png" type="image/png" />
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+}
